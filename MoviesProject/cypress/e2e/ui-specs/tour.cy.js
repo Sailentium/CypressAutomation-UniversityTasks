1b12ad7e5c7bd0ab480c3b81f2c91f1d4c35f4ab
“Sailentium” <walktoparadise6@gmail.com>
2022-07-17 17:37:31 +0300

    Removed unused files, added cosmetic changes

--- MoviesProject/cypress/e2e/ui-specs/tour.cy.js
+++ MoviesProject/cypress/e2e/ui-specs/tour.cy.js
@@ -1,41 +1,41 @@
 describe("Movie site is loading", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("Filter");
+  });
 
-    beforeEach(() => {
-        cy.visit("http://localhost:3000/");
-        cy.contains("Filter");
-    });
+  it("Page of the first movie loading correctly", () => {
+    cy.get("[data-testid='movies-list-movie']")
+      .first()
+      .then((movie) => {
+        const firstMovieUrl = movie.attr("href");
+        cy.get("[data-testid='movies-list-movie']").first().click();
+        cy.url().should("eq", "http://localhost:3000" + firstMovieUrl);
+        cy.url().should("include", firstMovieUrl);
+      });
+  });
 
-    it('Page of the first movie loading correctly', () => {
-        cy.get("[data-testid='movies-list-movie']").first().then((movie) => {
-            const firstMovieUrl = movie.attr("href");
-            cy.get("[data-testid='movies-list-movie']").first().click();
-            cy.url().should('eq','http://localhost:3000' + firstMovieUrl);
-            cy.url().should('include',firstMovieUrl);
-        })
-    });
+  it("Check that Filter exists and working", () => {
+    cy.get("input").should("exist");
+    cy.get("input").type("Testing");
+    cy.get("[data-testid='movies-list-movie']").should("not.exist");
+    cy.get("input").clear().type("Sonic");
+    cy.get("[data-testid='movies-list-movie']").should("exist");
+    cy.get("[data-testid='movies-list-movie']").should("have.length", 1);
+  });
 
-    it('Check that Filter exists and working', () => {
-        cy.get('input').should('exist');
-        cy.get('input').type('Testing');
-        cy.get("[data-testid='movies-list-movie']").should('not.exist');
-        cy.get('input').clear().type('Sonic');
-        cy.get("[data-testid='movies-list-movie']").should('exist');
-        cy.get("[data-testid='movies-list-movie']").should('have.length', 1);
-    })
+  it("Check that Filter exists and working", () => {
+    cy.get("input").should("exist");
+    cy.get("input").type("Testing");
+    cy.get("[data-testid='movies-list-movie']").should("not.exist");
+    cy.get("input").clear().type("Sonic");
+    cy.get("[data-testid='movies-list-movie']").should("exist");
+    cy.get("[data-testid='movies-list-movie']").should("have.length", 1);
+  });
 
-it('Check that Filter exists and working', () => {
-    cy.get('input').should('exist');
-    cy.get('input').type('Testing');
-    cy.get("[data-testid='movies-list-movie']").should('not.exist');
-    cy.get('input').clear().type('Sonic');
-    cy.get("[data-testid='movies-list-movie']").should('exist');
-    cy.get("[data-testid='movies-list-movie']").should('have.length', 1);
-})
-
-it('Correct number of movies loaded', () => {
-    cy.get("[data-testid='movies-list-movie']").should('have.length', 20);
-    cy.get("[data-testid='movies-list-movie']").should('exist');
-    cy.get("[data-testid='movies-full-movie']").should('not.exist');
+  it("Correct number of movies loaded", () => {
+    cy.get("[data-testid='movies-list-movie']").should("have.length", 20);
+    cy.get("[data-testid='movies-list-movie']").should("exist");
+    cy.get("[data-testid='movies-full-movie']").should("not.exist");
+  });
 });
-
-})
